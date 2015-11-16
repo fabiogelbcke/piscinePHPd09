8f5a741c5b3245072c7459de61f882c5057f5044
Fabio SCHUBERT-GELBCKE <fschuber@e2r4p23.42.fr>
2015-11-16 23:32:41 +0100

    ex02 fixed some shit

--- ex02/todo.js
+++ ex02/todo.js
@@ -1,6 +1,5 @@
 window.onload = function() {
 
-	// from http://www.w3schools.com/js/js_cookies.asp
 	function getCookie(cookieName) {
 		var name = cookieName + "=";
 		var cookieList = document.cookie.split(';');
@@ -15,8 +14,6 @@ window.onload = function() {
 		}
 		return null;
 	}
-
-	// from http://stackoverflow.com/questions/8097423/javascript-cookies-are-not-being-set
 	function setCookie(name, value, days)
 	{
 		if (days)
@@ -29,37 +26,25 @@ window.onload = function() {
 			var expires = "";
 		document.cookie = name + "=" + value + expires + ";path=/";
 	}
-
 	highestCookieValue = 0;
-
 	function addTask(text) {
 		console.log("adding task to DOM: " + text);
-
-		// add to cookies
 		highestCookieValue++;
 		itemId = "todo" + highestCookieValue;
-;
             setCookie(itemId, text, 10000);
-            
-            // create new <li> item with specified text
             var newItem = document.createElement("LI");
             newItem.id = itemId;
             newItem.appendChild(document.createTextNode(text));
-            
-            // adds click event to the li item (remove on click)
             newItem.addEventListener("click", function(eventObject) {
-                var areTheySure = confirm("Are you sure you want to delete this todo?");
+                var areTheySure = confirm("Delete this item?");
                 if (areTheySure) {
                     setCookie(eventObject.target.id, "", -1);
                     eventObject.target.remove();
                 }
             });
-            
-            // adds the new <li> to the DOM
             var list = document.getElementById("ft_list");
             list.insertBefore(newItem, list.childNodes[0]);
         }
-    
     var newButton = document.getElementById("add_item");
     newButton.addEventListener("click", function() {
         console.log("they clicked the button");
@@ -70,8 +55,6 @@ window.onload = function() {
             console.log("they hit cancel :(");
         }
     });
-    
-    // reload from cookies
     var cookieList = document.cookie.split(';');
     for(var i = 0; i < cookieList.length; i++) {
         var cookie = cookieList[i];
